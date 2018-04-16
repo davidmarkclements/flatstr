c201b13a7ccb92672de019c876631e037c600da2
davidmarkclements <huperekchuno@googlemail.com>
2018-04-16 15:24:11 +0200

    node 10 ready

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-module.exports = function flatstr(s) {
-  Number(s)
-  return s
+'use strict'
+
+try { 
+  var flatstr = Function('s', 'return %FlattenString(s)')
+} catch (e) {
+  try { 
+    var v8 = require('v' + '8')
+    v8.setFlagsFromString('--allow-natives-syntax')
+    var flatstr = Function('s', 'return %FlattenString(s)')
+    v8.setFlagsFromString('--no-allow-natives-syntax')
+  } catch (e) {
+    var flatstr = function flatstr(s) {
+      Number(s)
+      return s
+    }
+  }
 }
+
+module.exports = flatstr
